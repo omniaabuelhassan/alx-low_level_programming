dc96a3a26f857f6a5ae5c1b68c4bfa2c263e4671
omniaabuelhassan <omnya200489@gmail.com>
2023-04-23 15:31:11 -0700

    l

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - it is a main function and  in this program prints a line with puts function
+ * main - it is a main function
  *
  *Description: program that prints with put function
  *Rutern:always function return 0 if it success
