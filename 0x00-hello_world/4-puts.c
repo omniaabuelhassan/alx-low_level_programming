87c42d1a65ab50f36ba15eec6bf9001275dac95c
omniaabuelhassan <omnya200489@gmail.com>
2023-04-23 14:51:20 -0700

    l

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
-int main (void){
-	puts("\"Programming is like building a multilingual puzzle");	
-	return (0);}
+
+/**
+ * main -Entry point 
+ *
+ * Description: A C program that prints with put function 
+ *
+ * Rutern: always 0 (success) 
+ */
+
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
