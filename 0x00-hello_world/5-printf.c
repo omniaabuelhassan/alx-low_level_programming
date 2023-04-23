dd7da04e8288f5147e474418f23eb091efa82157
omniaabuelhassan <omnya200489@gmail.com>
2023-04-23 16:17:19 -0700

    l

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints with printf functin
+ *
+ * Return: 0 (succcess)
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
