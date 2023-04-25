5263c3be781be5253e1d87239cc1d8f05b825f85
omniaabuelhassan <omnya200489@gmail.com>
2023-04-24 19:48:12 -0700

    k

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,15 +7,13 @@
 
 int main(void)
 {
-int digit = 0;
-char ch = 'a';
-while (digit < 10)
+int digit = 48;
+while (digit <= 102)
 {
-putchar(digit + '0');
-}
-while (ch <= 'f')
-{
-putchar(ch);
+putchar(digit);
+if (digit == 57)
+	digit += 39;
+digit++;
 }
 putchar('\n');
 return (0);
