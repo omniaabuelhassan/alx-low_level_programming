97692a8468de3888125f52da7c1e8e50659ea594
omniaabuelhassan <omnya200489@gmail.com>
2023-04-24 19:12:32 -0700

    K

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: program that print digit from 0 to 9
+ * Return: 0(success)
+ */
+int main(void)
+{
+for (int digit = 0; digit < 10 ; digit++)
+{
+printf("%i", digit);
+}
+printf('\n');
+return (0);
+}
