620c98913570554ee1fe1bafff20c88b338912ca
omniaabuelhassan <omnya200489@gmail.com>
2023-05-03 19:35:30 -0700

    K

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *puts_half - print half of a string
+ *@str: string parameter
+*Return: nothing
+*/
+
+void puts_half(char *str)
+{
+int x;
+
+for (x = 0; str[x] != '\0'; x++)
+	x++;
+for (x /= 2; str[x] != '\0'; x++)
+{
+	_putchar(str[x]);
+}
+_putchar('\n');
+}
