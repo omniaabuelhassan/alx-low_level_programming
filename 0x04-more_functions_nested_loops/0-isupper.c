7699002c9a7625f7b5ad909797731bb432014ec0
omniaabuelhassan <omnya200489@gmail.com>
2023-05-07 10:10:37 -0700

    L

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,18 @@
 #include "main.h"
 /**
- * _isupper -check if c is upper
- * @c : input character
- * Return: 1(success) 0(unsuccess)
-  */
+* _isupper - checks for uppercase character
+* @c: Variable text
+* Return: Always 0.
+*/
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
-		Return(1);
-	else
-	Return(0);
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+
 }
--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,12 +1,13 @@
-#include <unistd.h>
 #include "main.h"
+#include <unistd.h>
 /**
  * _putchar - writes the character c to stdout
- * @c : the input character we print
+ * @c: The character to print
  *
- * Return : 1 (success) -1 (unsuccess)
- * */
-int _putchar (char c)
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
 {
 return (write(1, &c, 1));
 }
